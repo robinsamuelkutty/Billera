cdded18f557fc655cf74d3b2cb7b155222399a4e
Robin Samuelkutty <robinsam754@gmail.com>
2025-06-04 17:29:36 +0530

    setup started

---
+++ backend/src/main/java/in/robinsam/bilingsoftware/BilingsoftwareApplication.java
@@ -0,0 +1,13 @@
+package in.robinsam.bilingsoftware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilingsoftwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BilingsoftwareApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/in/robinsam/bilingsoftware/BilingsoftwareApplicationTests.java
@@ -0,0 +1,13 @@
+package in.robinsam.bilingsoftware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilingsoftwareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
